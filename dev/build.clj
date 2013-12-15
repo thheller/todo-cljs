a98afb07e6b5c4b243bbd84ae7ed5976fd08ff3f
Thomas Heller <info@zilence.net>
2013-12-15 23:05:57 +0100

    stuff

---
+++ dev/build.clj
@@ -0,0 +1,31 @@
+(ns build
+  (:require [shadow.cljs.build :as cljs]
+            [clojure.java.io :as io]))
+
+;; hmm dogfood
+
+(defn dev
+  "build the project, wait for file changes, repeat"
+  [& args]
+  (let [state (-> (cljs/init-state)
+                  (cljs/enable-source-maps)
+                  (assoc :optimizations :none
+                         :pretty-print true
+                         :work-dir (io/file "target/cljs-work")
+                         :public-dir (io/file "public/assets/cljs")
+                         :public-path "/assets/cljs")
+                  (cljs/step-find-resources-in-jars)
+                  (cljs/step-find-resources "src/cljs")
+                  (cljs/step-find-resources "checkouts/shadow/src/cljs")
+                  (cljs/step-finalize-config)
+                  (cljs/step-compile-core)
+                  (cljs/step-configure-module :app ['todo-cljs.app] #{})
+                  )]
+    
+    (loop [state state]
+      (recur (-> state
+                 (cljs/step-compile-modules)
+                 (cljs/flush-unoptimized)
+                 (cljs/wait-and-reload!)
+                 ))))
+  :done)
---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject todo-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [thheller/shadow-client "0.1.0"]]
+
+
+  :profiles {:dev {:source-paths ["dev"]
+                   :dependencies [[org.clojure/clojurescript "0.0-2120" :exclusions [org.mozilla/rhino]]
+                                  [thheller/shadow-build "0.1.0"]]}}
+
+  :source-paths ["src/clj" "src/cljs"]
+  )
